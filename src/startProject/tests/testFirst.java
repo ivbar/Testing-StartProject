258448b6a11fc844449b711466255370b267c425
ivbar <ivbar@list.ru>
2012-01-10 17:51:51 +0200

    New web driver ver Move TemplateTest.xml

--- src/startProject/tests/testFirst.java
+++ src/startProject/tests/testFirst.java
@@ -1,35 +1,29 @@
 package startProject.tests;
 
-
 import org.testng.annotations.Test;
 
 import startProject.TestHelper;
 import startProject.pageObjects.PageHome;
 
-
-
 public class testFirst extends TestHelper {
-	
+
 	/**
 	 * Creating test data
 	 */
-//	@DataProvider(name = "dp")
-//	public Object[][] createData1() {
-//	 return new Object[][] {
-//	   { "http://painclinician.com/"}
-//	 };
-//	}
+	// @DataProvider(name = "dp")
+	// public Object[][] createData1() {
+	// return new Object[][] {
+	// { "http://painclinician.com/"}
+	// };
+	// }
 
 	/**
 	 * Bay standard order
 	 */
 	@Test
-//	(dataProvider = "dp")
+	// (dataProvider = "dp")
 	public void makeOrder() {
-		new PageHome()
-		.clickProgects()
-		;
+		new PageHome().clickProgects();
 	}
-	
 
 }
